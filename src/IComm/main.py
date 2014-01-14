08cafe1bbb0aa8713d0b7f8982e9688f245cc5ac
Louise Crepet <louise.crepet@insa-lyon.fr>
2014-01-14 11:43:18 +0100

    Sketch of connection page

--- src/IComm/main.py
+++ src/IComm/main.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     monTexte = 'Hello World'
     return render_template('index.html', helloworld=monTexte)
+	
+@app.route('/connection')
+def connection():
+    monTexte = 'Connection'
+    return render_template('connection.html', helloworld=monTexte)
 
 if __name__ == '__main__':
     app.run(debug=True)

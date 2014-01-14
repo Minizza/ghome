7530c3aefbe7e61e0b69af10dd327da9914e8562
tgermain <tim@timCrunchBang>
2014-01-14 13:41:33 +0100

    comprendre les trames !

--- src/traducteur/traductor.py
+++ src/traducteur/traductor.py
@@ -4,9 +4,8 @@
 #st="A55A0B06000000080001B25E002A"
 #st="0"
 #print(' '.join(format(x, 'b') for x in bytearray(st)))
-
-my_hexdata = "A55A0B06000000090001B25E002B"
-scale = 16 ## equals to hexadecimal
+my_hexdata = "A55A0B05000000000021CBE320FF"
+scale = 16 ## equals to hexadecimalœ
 num_of_bits = len(my_hexdata)*4
 print(bin(int(my_hexdata, scale))[2:].zfill(num_of_bits))
 

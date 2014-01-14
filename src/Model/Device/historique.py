db6944cc1bf603bb62bf3319801498e1de11e19a
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-01-14 09:25:18 +0100

    Updated historique.py

--- src/Model/Device/historique.py
+++ src/Model/Device/historique.py
@@ -4,5 +4,8 @@ from mongoengine import *
 
 class Historique(Document):
 	"""Classe mère pour les périphériques
-			- date : la liste des dates de mesure des états du périphérique
-			- etat : la liste des états du périphériques"""
+		- date : la liste des dates de mesure des états du périphérique
+		- etat : la liste des états du périphériques"""
+			
+	date = ListField(DateTimeField())
+	etat = ListField(DynamicField())

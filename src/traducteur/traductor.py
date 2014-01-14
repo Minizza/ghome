0c742f67681ee087af964728ffae03e76b5c45b6
tgermain <tim@timCrunchBang>
2014-01-14 13:32:16 +0100

    on continue de comprendre les trames

--- src/traducteur/traductor.py
+++ src/traducteur/traductor.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-          
 
-from pants import Engine, Server, Stream
+#st="A55A0B06000000080001B25E002A"
+#st="0"
+#print(' '.join(format(x, 'b') for x in bytearray(st)))
 
+my_hexdata = "A55A0B06000000090001B25E002B"
+scale = 16 ## equals to hexadecimal
+num_of_bits = len(my_hexdata)*4
+print(bin(int(my_hexdata, scale))[2:].zfill(num_of_bits))
 
-class Echo(Stream):
-    def on_read(self, data):
-        self.write(data)
-
-
-
-
-if __name__ == '__main__':
-    print ("Début")
-    server = Server(ConnectionClass=Echo)
-    server.listen(4040)
-    Engine.instance().start()

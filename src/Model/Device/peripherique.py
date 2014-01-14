f0649c2ee5567429fdcedd49df50e3ad6a3b8e26
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-01-14 08:54:28 +0100

    Add Device package and renamed class files

---
+++ src/Model/Device/peripherique.py
@@ -0,0 +1,15 @@
+# -*-coding:Utf-8 -*
+
+from mongoengine import *
+
+
+class Peripherique(Document):
+    """Classe mère pour les périphériques
+        - id_logique : l'id du périphérique dans l'application
+        - id_physique : l'id physique du périphérique
+        - historique : l'historique des états du périphérique
+        - etat_courant : l'état courant du périphérique"""
+        
+    id_logique = IntField(required = True)
+    id_physique = StringField(required = True)
+    #historique = ReferenceField(Historique)

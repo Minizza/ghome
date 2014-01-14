319fa2cf2f13ed4acc3ccae553b4e6b6e761fc53
Julien <julien@ju-s.(none)>
2014-01-14 09:19:15 +0100

    try gitignore

---
+++ src/traducteur/fakeJerome.py
@@ -0,0 +1,31 @@
+# -*- coding : utf-8 -*-
+
+from pants import Engine, Server, Stream
+
+class Echo(Stream):
+    def on_read(self, data):
+        self.write(data)
+    def balancer(data):
+        self.write(data)
+
+def balTemp ():
+    data = 'A55A'
+    Stream.balancer(data)
+
+def balCont ():
+    data = 'A55A'
+
+def balPres ():
+    data = 'A55A'
+
+def main () :
+    print 'fakeJerome : concepteur de fakeTrames'
+    while 1 : 
+        print 'Que voulez vous balancer ? (tavu ?)\n'
+        print '1 : Capteur temperature'
+        print '2 : Capteur contact'
+        print '3 : Capteur presence'
+        kloug = raw_input('A vous :')
+        case kloug :
+
+main()    

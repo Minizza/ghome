06113975de9ffb2f88c61148c4d6b80bd10850a1
Ju <ju@ju-1001PX.(none)>
2014-01-15 14:29:01 +0100

    Slicing basique

--- src/traducteur/traductor.py
+++ src/traducteur/traductor.py
@@ -9,3 +9,30 @@ scale = 16 ## equals to hexadecimalœ
 num_of_bits = len(my_hexdata)*4
 print(bin(int(my_hexdata, scale))[2:].zfill(num_of_bits))
 
+
+
+
+#Ici on reçoit la trame 
+receivedData = "A55A0B05000000000021CBE320FF"
+
+#Ici, on traite la trame (slicing)
+sep = receivedData[:4]
+lenght = receivedData[4:6]
+rOrg = receivedData[6:8]
+data1 = receivedData[8:10]
+data2 = receivedData[10:12]
+data3 = receivedData[12:14]
+data4 = receivedData[14:16]
+ident = receivedData[16:24]
+flag = receivedData[24:26]
+checkSum = receivedData[26:]
+print sep
+print lenght
+print rOrg
+print data1
+print data2
+print data3
+print data4
+print ident
+print flag
+print checkSum

883ec4207b456733ee42649265e31a689cda9d0a
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-01-21 10:48:05 +0100

    Corrected bug in place.py

--- src/Model/Place/place.py
+++ src/Model/Place/place.py
@@ -5,26 +5,27 @@ from mongoengine import *
 import sys
 import ghomeuser
 
-path = "../Model/Device/"
+path = "../Device/"
 if path not in sys.path:
 	sys.path.append(path)
 
-path = "../Model/Draw/"
+import device
+
+path = "../Draw/"
 if path not in sys.path:
 	sys.path.append(path)
 
-from device import *
-from draw import *
+import draw
 
 
 class Place(Document):
 		
 	id = ObjectIdField(required = True) # Id requis
 	name = StringField(max_length = 20, required = True)
-	draw = ReferenceField(Draw)
+	draw = ReferenceField(draw.Draw)
 	maxX = IntField()
 	maxY = IntField()
 	maxZ = IntField()
 	users = ListField(ReferenceField(ghomeuser.GHomeUser))
-	devices = ListField(ReferenceField(Device))
+	devices = ListField(ReferenceField(device.Device))
 	

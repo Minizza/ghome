e491dce749b84d346e3a2da427f620411b2c9f2a
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-01-21 10:44:23 +0100

    Updated tests for Model

--- src/tests/model.py
+++ src/tests/model.py
@@ -1,62 +1,70 @@
 # -*-coding:Utf-8 -*
 
-import sys
-path = "../Model/Device/"
-sys.path.append(path)
-
-from device import *
-from sensor import *
-from actuator import *
-from historic import *
-
-path = "../Model/User/"
-sys.path.append(path)
-
-from user import *
-
 from mongoengine import *
+
+import sys
+
+path = "../Model/Device/"
+if path not in sys.path:
+	sys.path.append(path)
+
+import device
+import sensor
+import actuator
+import historic
+	
+path = "../Model/Place"
+if path not in sys.path:
+	sys.path.append(path)
+
+import place
+import ghomeuser
 
 connect('test')
 
 
 ########################################################################
-# Tests for Utilisateur base
+# Tests for User base
 ########################################################################
 def user_test():
 	#Deleting pre-existing users to clean the test database
-	for user in User.objects:
-		user.delete()
+	ghomeuser.GHomeUser.drop_collection()
 
-	user1 = User(name = "Dupont", password = "test", role = "Basic")
-	user2 = User(name = "Martin", password = "pass", role = "Admin")
+	user1 = ghomeuser.GHomeUser(name = "Dupont", password = "test", role = "Basic")
+	user2 = ghomeuser.GHomeUser(name = "Martin", password = "pass", role = "Admin")
 
 	#Adding new users
 	user1.save()
 	user2.save() 
 
 	#Invalid user: name is missing (uncomment to test)
-	#user3 = User(password = "f*ck", role = "Basic")	
+	#user3 = GHomeUser(password = "f*ck", role = "Basic")	
 	#user3.save()
 		
 	#Printing users in Utilisateur base
-	for user in User.objects:
+	for user in ghomeuser.GHomeUser.objects:
 		print user.id, " ", user.name, " ", user.password, " ", user.role
 ########################################################################
 
 
 ########################################################################
-# Tests for Peripherique base
+# Tests for Device base
 ########################################################################
-def peripherique_test():
+def device_test():
 	#Deleting pre-existing peripherique to clean the test database
-	for device in Device.objects:
-		device.delete()
+	device.Device.drop_collection()
 		
-	capteur1 = Sensor(physic_id = "12230EAF", current_state = BooleanField())
-	capteur1.current_state = False
+	capteur1 = sensor.Sensor(physic_id = "12230EAF", name = "CAPTEUR1_CUISINE", current_state = 19)
 	capteur1.save()
 	
-	for device in Device.objects:
-		print device.physic_id, " ", current_state
+	actuator1 = actuator.Actuator(physic_id = "4562312AAA", name = "ACTIONNEUR2_TOILETTE", current_state = "OPEN")
+	actuator1.save()
+	
+	#Invalid device: duplicate ID (uncomment to test)
+	#actuator2 = Actuator(physic_id = "12230EAF", name = "ACTIONNEUR_WRONG", current_state = "CLOSED")
+	#actuator2.save()
+	
+	for device in device.Device.objects:
+		print device.physic_id, " ", device.name, " ", device.current_state
 	
 ########################################################################

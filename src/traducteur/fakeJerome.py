e6690059234c1b0150bfd14b8e17bb7305a04bad
tgermain <tim@timCrunchBang>
2014-01-21 10:13:44 +0100

    ajout d'un mode auto

--- src/traducteur/fakeJerome.py
+++ src/traducteur/fakeJerome.py
@@ -1,6 +1,7 @@
 # -*- coding : utf-8 -*-
 
 import socket
+import time
 
 """
 fakeJerome est un simple support d'envoi de fausses trames pour tester
@@ -21,9 +22,28 @@ def balCont (c):
 def balPres (c):
     data = 'A55A0B079DB8000D0004E59500F2'
     c.send(data)
-    
-def main () :
+
+def auto () :
     print 'fakeJerome : concepteur de fakeTrames'
+    print 'Auto mode : send random trames every 5s'
+    chan = server.bind(('', 1515))    
+    server.listen(10000)  
+    c,adrr = server.accept()      
+    while 1 : 
+        test = random.randint(1,3)
+        if test == 1 :
+            balTemp(c)
+        elif test == 2 :
+            balCont(c)
+        elif test == 3 :
+            balPres(c)
+        else : 
+            raise AttributeError
+        time.sleep(5)
+
+def manual () :
+    print 'fakeJerome : concepteur de fakeTrames'
+    print 'Manual mode'
     chan = server.bind(('', 1515))    
     server.listen(10000)  
     c,adrr = server.accept()      
@@ -43,4 +63,7 @@ def main () :
         else : 
             print 'Haha petit malin !'
 
-main()    
+
+if __name__ == '__main__':
+	#manual()
+	auto() 

09f6c441100083089c4852148c8c279dd8573362
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-01-27 16:48:13 +0100

    Updated Model tests

--- src/tests/model.py
+++ src/tests/model.py
@@ -23,6 +23,7 @@ def user_test():
 	#Deleting pre-existing users to clean the test database
 	ghomeuser.GHomeUser.drop_collection()
 
+	#user1
 	user1 = ghomeuser.GHomeUser(name = "Dupont", password = "test", role = "Basic")
 	user2 = ghomeuser.GHomeUser(name = "Martin", password = "pass", role = "Admin")
 
@@ -46,33 +47,36 @@ def user_test():
 def device_test():
 	#Deleting pre-existing devices to clean the test database
 	ghomedevice.Device.drop_collection()
-		
+	
+	#Setting a date field and a state field
+	dateField = [datetime.datetime.now(), datetime.datetime.now()]
+	stateField = [25, 22]
+	
+	#capteur1: no historic at creation, uses add_state() and setCurrentState()	
 	capteur1 = sensor.Sensor(physic_id = "12230EAF", name = "CAPTEUR1_CUISINE", current_state = 19)
 	capteur1.save()
+	capteur1.addState(datetime.datetime.now(), capteur1.current_state)
+	capteur1.setCurrentState(20)
 	
+	#actuator1: no historic at creation, uses setHistoric()
 	actuator1 = actuator.Actuator(physic_id = "4562312AAA", name = "ACTIONNEUR2_TOILETTE", current_state = "OPEN")
 	actuator1.save()
+	historic1 = historic.Historic(date = dateField, state = stateField)
+	actuator1.setHistoric(historic1)
 	
 	#Invalid device: duplicate ID (uncomment to test)
 	#actuator2 = actuator.Actuator(physic_id = "12230EAF", name = "ACTIONNEUR_WRONG", current_state = "CLOSED")
 	#actuator2.save()
 	
-	dateField = [datetime.datetime.now(), datetime.datetime.now()]
-	stateField = [25, 22]
-	historic1 = historic.Historic(date = dateField, state = stateField)
-	
-	historic1.save()
-	
-	capteur2 = sensor.Sensor(physic_id = "AEFF4242", name = "CAPTEUR2_CUISINE", current_state = 22, historic = historic1)
-	capteur2.save()
-	
-	capteur2.addState(datetime.datetime.now(), 27)
-	
 	historic2 = historic.Historic(date = dateField, state = stateField)
-	actuator1.setHistoric(historic2)
+	historic2.save()
 	
-	capteur1.addState(datetime.datetime.now(), 19)
+	#capteur2: historic at creation, use addState()
+	capteur2 = sensor.Sensor(physic_id = "AEFF4242", name = "CAPTEUR2_CUISINE", current_state = 22, historic = historic2)
+	capteur2.save()
+	capteur2.addState(datetime.datetime.now(), 27)
 
+	#Print all devices' info'
 	for device in ghomedevice.Device.objects:
 		print device.physic_id, " ", device.name, " ", device.current_state
 		if device.historic:

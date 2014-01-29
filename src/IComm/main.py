862a45f0963c45c6952ab6e8e9691f507417e578
QuentinBonnet <quentin.bonnet.work@gmail.com>
2014-01-29 14:37:35 +0100

    Added fancy bootstrap tab on devices page

--- src/IComm/main.py
+++ src/IComm/main.py
@@ -19,71 +19,71 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 # Decorator to check roles
 def requires_roles(*roles):
-	def wrapper(f):
-		@wraps(f)
-		def wrapped(*args, **kwargs):
-			if get_current_user_role() not in roles:
-				return error('privileges')
-			return f(*args, **kwargs)
-		return wrapped
-	return wrapper
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if get_current_user_role() not in roles:
+                return error('privileges')
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
 
 # Authentication
 def get_current_user_role():
-	if 'role' in session:
-		return session['role']
-	return None
+    if 'role' in session:
+        return session['role']
+    return None
 
 def set_current_user_role(role):
-	session['role'] = role
+    session['role'] = role
 
 # Routes
 
 @app.route('/')
 def index():
-	monTexte = "Ceci est la page d'accueil du site"
-	return render_template('index.html', data=monTexte)
-	
+    monTexte = "Ceci est la page d'accueil du site"
+    return render_template('index.html', data=monTexte)
+    
 @app.route('/connection')
 def connection():
     return render_template('connection.html')
-	
+    
 
 @app.route('/connection', methods=["POST"])
 def connection_post():
-	username = request.form['username']
-	password = request.form['password']
-	
-	connect('test')
-	find = 0
-	for user in ghomeuser.GHomeUser.objects:
-		if user.name == username:
-			if user.password == password:
-				find = 1
-				session['role'] = user.role
-	
-	if find:
-		return render_template('index.html')
-	else : 
-		return render_template('connection.html')
+    username = request.form['username']
+    password = request.form['password']
+    
+    connect('test')
+    find = 0
+    for user in ghomeuser.GHomeUser.objects:
+        if user.name == username:
+            if user.password == password:
+                find = 1
+                session['role'] = user.role
+    
+    if find:
+        return render_template('index.html')
+    else : 
+        return render_template('connection.html')
 
 @app.route('/devices')
 @requires_roles('admin')
 def devices():
     connect('test')
     devices = ghomedevice.Device.objects # Fetch les devices depuis la BD ici !
-	return render_template('devices.html', devices=devices)
+    return render_template('devices.html', devices=devices)
 
 @app.route('/logout')
 def logout():
-	session.pop('role', None)
-	return connection()
+    session.pop('role', None)
+    return connection()
 
 @app.route('/error/<type>')
 def error(type):
-	if type == 'privileges':
-		return render_template('error_privileges.html')
-	return index()
+    if type == 'privileges':
+        return render_template('error_privileges.html')
+    return index()
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)

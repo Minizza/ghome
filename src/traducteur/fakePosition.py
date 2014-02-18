991537dc496967799170c7e6e1cfdd372f89acfd
tgermain <timothee.germain@wanadoo.fr>
2014-02-18 11:44:06 +0100

    Début du fake positionneur avec translation des coord et production de la bonne trame

--- src/traducteur/fakePosition.py
+++ src/traducteur/fakePosition.py
@@ -1,15 +1,42 @@
 # -*-coding:Utf-8 -*
 
 from mongoengine import *
+import trame
 
 class fakePosition():
 
-	position = [0, 0, 0]
+    def __init__(self):
+        self.positionAbs = {'x':0, 'y':0}
+        self.maxX=500
+        self.maxY=500
+        self.start = "A55A4242"
+        #dataBytes
+        self.ident = "12345601"
+        self.end ="FF"
 
-	def setPosition(self, x, y, z):
-		self.position = [x, y , z]
+    def update(self,absX,absY):
+        newCoord=self.translateCoord(absX,absY)
+        strTrame=self.start+newCoord.get('x')+newCoord.get('y')+self.ident+self.end
+        print strTrame
+        myTrame=trame.trame(strTrame)
 
 
-	def sendTrame():
-		trame = ""
-		return trame
+    def translateCoord(self,absX,absY):
+        """
+        convert absolute coordonate to 4 Bytes value
+        """
+        rawConvertedX=hex(absX*(16**4-1)/self.maxX)
+        rawConvertedY=hex(absY*(16**4-1)/self.maxY)
+        return {'x':rawConvertedX[2:],'y':rawConvertedY[2:]}
+
+    def sendTrame():
+        """
+        Create the trame from calculated cooordonates
+        """
+   
+def main():
+    blarg=fakePosition()
+    blarg.update(500,500)
+
+if __name__ == '__main__':
+       main()   

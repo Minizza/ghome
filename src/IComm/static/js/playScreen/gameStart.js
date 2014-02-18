f771536d175171b5fc70389f29485372c3ebaa8f
Julien <julien@ju-s.(none)>
2014-02-18 11:42:04 +0100

    Corrections gameStart boucles

--- src/IComm/static/js/playScreen/gameStart.js
+++ src/IComm/static/js/playScreen/gameStart.js
@@ -90,7 +90,7 @@ function updateData ()
 
                             if (parseInt(data[i].state) == 1)
                             {
-                                for (var j=0; i<bddAllies.length; j++)
+                                for (var j=0; j<bddAllies.length; j++)
                                 {
                                     if (data[i].ident == bddAllies[j].ident)
                                     {
@@ -103,7 +103,7 @@ function updateData ()
                             }
                             else if (parseInt(data[i].state) == 2)
                             {
-                                for (var j=0; i<bddEnemies.length; j++)
+                                for (var j=0; j<bddEnemies.length; j++)
                                 {
                                     if (data[i].ident == bddEnemies[j].ident)
                                     {
@@ -118,7 +118,7 @@ function updateData ()
                         }
                         else if ((data[i].type == "Switch")||(data[i].type == "Temperature"))
                         {
-                            for (var j=0; i<bddCapteurs.length; j++)
+                            for (var j=0; j<bddCapteurs.length; j++)
                                 {
                                     if (data[i].ident == bddCapteurs[j].ident)
                                     {

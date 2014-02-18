b757a8971866b39aae9f1cd17d32f60f0919ee5a
Louise Crepet <louise.crepet@insa-lyon.fr>
2014-02-18 10:39:25 +0100

    Player sends coordinates

--- src/IComm/static/js/playScreen/gamePlayer.js
+++ src/IComm/static/js/playScreen/gamePlayer.js
@@ -1,17 +1,29 @@
-var player;
+$(function() {
 
 function GamePlayer() {
 
+	//Creation of player
 	this.setup = function()
 	  {
 	    player = new jaws.Sprite({ image:"../static/medias/player.png" });
 	    var playerSpeed = 4;
+		
+		//Player up in the middle
 		player.x = 300;
 	    player.y = 40;
+		
+		function SendCoordinates() {
+			$.post( "testplayer/location", { abscissa: player.x, ordinate: player.y }, function( data ) {
+				setTimeout(SendCoordinates, 2000);
+			});
+		}
+		
+		SendCoordinates();
 	  }
 
 	this.update = function() { 
 
+		//Moves if button pressed
 		if (jaws.pressed("left")) {
 	      player.x -= 3;
 	    }
@@ -25,6 +37,7 @@ function GamePlayer() {
 	      player.y += 3;
 	    }
 		
+		//Player can't leave square
 		if (player.x < 35)
 	    {
 			player.x = 35;
@@ -32,6 +45,7 @@ function GamePlayer() {
 		if (player.x > 610)
 	    {
 			player.x = 610;
+
 	    }
 		if (player.y < 40)
 	    {
@@ -41,8 +55,10 @@ function GamePlayer() {
 	    {
 			player.y = 545;
 	    }
+		
 	}
 	
+	//Draw !
 	this.draw = function()
 	  {
 	    jaws.context.clearRect(0, 0, jaws.width, jaws.height);
@@ -51,7 +67,10 @@ function GamePlayer() {
 	
 }
 
+//When the page is loaded : image and function loaded too
 jaws.onload = function() {
-      jaws.assets.add("../static/medias/player.png")
-      jaws.start(GamePlayer)
+      jaws.assets.add("../static/medias/player.png");
+      jaws.start(GamePlayer);
+	  
 };
+});

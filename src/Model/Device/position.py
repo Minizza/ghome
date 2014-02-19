3587e24a55f5f203f21a149944af795ea63d37a2
tgermain <timothee.germain@wanadoo.fr>
2014-02-19 15:31:46 +0100

    deplacement de la logique de traduction des trames dans le modèle

--- src/Model/Device/position.py
+++ src/Model/Device/position.py
@@ -9,3 +9,12 @@ class Position(Sensor):
 	maxY=545
 	trameStart="A55A4242"
 	trameEnd ="FF"
+
+	def translateTrame(self,inTrame):
+		"""
+
+		"""
+		print inTrame.lessRawView()
+		rawConvertedX= int((inTrame.data1+inTrame.data0),16)
+		rawConvertedY=int((inTrame.data3+inTrame.data2),16)
+		absX=rawConvertedX/(16**4-1.0)*self.maxX
--- src/Model/Device/switch.py
+++ src/Model/Device/switch.py
@@ -1,5 +1,25 @@
 # -*-coding:Utf-8 -*
 from sensor import *
- 
+from logger import loggerConfig
+
+logger=loggerConfig.configure()
+
 class Switch(Sensor):
 	"""Switch class"""
+
+	def translateTrame(self,inTrame):
+		"""
+		return 	close if data0=09, 
+				open if data0=08 
+				else nothing
+		"""
+		if (inTrame.data0=='09'):
+                        logger.info("Door sensor {} with state [close]".format(inTrame.ident))
+                        dataToRet = "close"
+	        elif(inTrame.data0=='08'):
+	            logger.info("Door sensor {} with state [open]".format(inTrame.ident))
+	            dataToRet = "open"
+	        else:
+	            logger.warn("Door sensor {}Strange state : {}".format(inTrame.ident, inTrame.data2))
+	            dataToRet=''
+		return dataToRet

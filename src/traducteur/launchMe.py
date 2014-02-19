40a85e3a17ad9e1629c63787b98606f8b5918931
tgermain <timothee.germain@wanadoo.fr>
2014-02-19 11:41:57 +0100

    nettoyage de launchMe (on va ssans doute faire du multiprocess)

--- src/traducteur/launchMe.py
+++ src/traducteur/launchMe.py
@@ -7,15 +7,10 @@ from mongoengine import *
 addr='134.214.106.23'
 port=5000
 
-def addCaptor():
-    """
-    """
 
 def main():
     connect('test')
-    addCaptor()
-    myTrad=traductor()
-    myTrad.launch(addr,port)
+    myTrad=traductor().launch(addr,port)
 
 
 if __name__ == '__main__':

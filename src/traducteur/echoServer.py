2c4f02ca89b2f4bc0dbd2c390ee00daf775a45ce
tgermain <timothee.germain@wanadoo.fr>
2014-02-23 22:32:50 +0100

    mon serveur il rooooXXX

---
+++ src/traducteur/echoServer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import socket
+import thread
+
+clientList=[]
+def handler(clientsock,addr):
+       while 1:
+            data = clientsock.recv(BUFSIZ)
+            if not data: break
+            for client in clientList:
+                client.send("echoed:.."+data)
+       clientsock.close()
+
+
+if __name__=='__main__':
+        HOST = 'localhost'
+        PORT = 1515
+        BUFSIZ = 1024
+        ADDR = (HOST, PORT)
+        serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serversock.bind(ADDR)
+        serversock.listen(10)
+
+        while 1:
+            print 'waiting for connection…'
+            clientsock, addr = serversock.accept()
+            clientList.append(clientsock)
+            print '…connected from:', addr
+            thread.start_new_thread(handler, (clientsock, addr))
+#some other cleanup code if necessary

8803990d12980a068e1ac9215c3f6cfa7ec1e057
tgermain <timothee.germain@wanadoo.fr>
2014-02-24 19:51:47 +0100

    Gestion des identifiants des actuator

--- src/Model/Device/DeviceFactory.py
+++ src/Model/Device/DeviceFactory.py
@@ -16,7 +16,10 @@ class DeviceFactory(object):
         elif type == "position":
             new = Position(x=Device.UNDEFINED_COORDINATE, y=Device.UNDEFINED_COORDINATE, physic_id=physic_id, name=name)
         elif type == "actuator":
-            new = Actuator(x=Device.UNDEFINED_COORDINATE, y=Device.UNDEFINED_COORDINATE, physic_id=physic_id, name=name)
+            if (len(physic_id)==1):
+                new = Actuator(x=Device.UNDEFINED_COORDINATE, y=Device.UNDEFINED_COORDINATE, physic_id="FF9F1E0"+physic_id, name=name)
+            else:
+                raise TypeError("Identificator too long ({}), you only need the plug number.".format(physic_id))
         else:
             raise TypeError("Bad device creation, type " + type + " doesn't exist !")
         return new

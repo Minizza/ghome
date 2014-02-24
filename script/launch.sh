401094da27b2568345d9f86fc7ac388abcf35067
Nicolas Buisson <nicolas.buisson@insa-lyon.fr>
2014-02-24 16:52:17 +0100

    Removed -f option for launch. Why? Because I can.

--- script/launch.sh
+++ script/launch.sh
@@ -15,19 +15,7 @@ CONFIG_PATH=$ICOMM_PATH"server/config.json"
 LAUNCH_PATH=$ICOMM_PATH"launch.py"
 RUNSERVER_PATH=$ICOMM_PATH"runserver.py"
 
-if [[ "$1" = "-f" ]]	# If fake launching
-then
-		sed -i "s/\"platforme\":.*/\"platforme\":\"127.0.0.1\",/" $CONFIG_PATH
-		sed -i "s/\"platformePort\":.*/\"platformePort\":1515,/" $CONFIG_PATH
-		if [[ $2 -eq 2 ]]	# If a terminal is precised
-		then
-		 	$2 -hold -e python $ECHO_PATH &
-		 	$2 -hold -e python $FAKEJ_PATH &
-		else	# If no terminal is precised, use xterminal
-			xterm -hold -e python $ECHO_PATH &
-			xterm -hold -e python $FAKEJ_PATH &
-	 	fi
-elif [ "$1" = "-j" ] # If true Jerome launching
+if [ "$1" = "-j" ] # If true Jerome launching
 then
 		sed -i "s/\"platforme\":.*/\"platforme\":\"134.214.106.23\",/" $CONFIG_PATH
 		sed -i "s/\"platformePort\":.*/\"platformePort\":5000,/" $CONFIG_PATH
